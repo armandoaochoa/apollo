5513ed0820fc9dad7c2966a9a520d7c06b5b7aa3
Armando Ochoa <armando.ochoa92@gmail.com>
2022-01-17 22:43:58 -0800

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,18 @@ class Config(object):
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-  FILESTACK_API_KEY = os.environ.get('FILESTACK_API_KEY') or 'Aav2WFMRdTlqC2x0zpLZkz'
-  FILESTACK_APP_SECRET = os.environ.get('FILESTACK_APP_SECRET') or 'ISYEHH4PIFEG7I44B77ZS3ALFE'
-  FILESTACK_WEBHOOK_SECRET = os.environ.get('FILESTACK_WEBHOOK_SECRET') or 'wpE5zmVSSDK72cUAQMfh'
+  FILESTACK_API_KEY = os.environ.get('FILESTACK_API_KEY')
+  FILESTACK_APP_SECRET = os.environ.get('FILESTACK_APP_SECRET')
+  FILESTACK_WEBHOOK_SECRET = os.environ.get('FILESTACK_WEBHOOK_SECRET')
 
-  SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY') or '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'
+  SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
 
   TEMPORARY_FILE_PATH = os.environ.get('TEMPORARY_FILE_PATH') or './temp_watermark_ops/'
 
-  EMAIL_FROM = os.environ.get('EMAIL_FROM') or 'armando.ochoa92@gmail.com'
+  EMAIL_FROM = os.environ.get('EMAIL_FROM')
 
   DOMAIN = os.environ.get('DOMAIN') or 'http://127.0.0.1:5000'
 
   PROD = os.environ.get('PROD') or 'production' # 'development'
 
-  #REDIS_URL = 'redis://localhost:6379'
 
